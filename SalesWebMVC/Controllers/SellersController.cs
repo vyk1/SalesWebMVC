7a420ba22b0fae22b61386ab38d895715e22a77b
vyk1 <victoriabotelho14@gmail.com>
2020-02-20 19:08:07 -0300

    Formulário para Sellers

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Services;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers {
     public class SellersController : Controller {
@@ -16,6 +17,18 @@ namespace SalesWebMVC.Controllers {
         public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller s) { 
+            _sellerService.Insert(s);
+            return RedirectToAction(nameof(Index));
+            //return RedirectToAction("Index");
         }
     }
 }
