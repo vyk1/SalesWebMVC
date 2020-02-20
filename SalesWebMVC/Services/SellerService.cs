d3e0a1cdcd957b882bbed6ecf03137096b9a0d78
vyk1 <victoriabotelho14@gmail.com>
2020-02-20 20:18:15 -0300

    Foreign key (First()) de Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -17,6 +17,7 @@ namespace SalesWebMVC.Services {
             return _context.Seller.ToList();
         }
         public void Insert(Seller obj) {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
