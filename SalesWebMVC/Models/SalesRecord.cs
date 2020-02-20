9cd9487615bb2e8391736c5ea777684699112ef9
vyk1 <victoriabotelho14@gmail.com>
2020-02-20 10:51:24 -0300

    migrações de SalesRecord, Seller

---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+        // no construtor, só não pode instanciar as coleções!
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
